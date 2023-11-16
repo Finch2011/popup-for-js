99d6893aa8006f335fae1ffe34444efd48de754a
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2023-11-16 12:08:10 -0800

    add code for java script = body.style.backgruoncolor = '#000' and body.style.backgroundcolor = '#fff' , add code for css = #div{ box-shado:14px 14x 100px ;} and #clickMe-style{  }

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 const clickMe = document.getElementById('clickMe')
 const close = document.getElementById('close')
 const div = document.getElementById('div')
+const body = document.getElementById('body')
 function getPopup(){
     div.style.top = "0px"
-    clickMe.style.top = "340px"
+    clickMe.style.top = "-400px"
+    body.style.backgroundColor = "#000"
+    
 }
 function closePopup(){
-    div.style.top = "-400px"
+    div.style.top = "-500px"
     clickMe.style.top = "0px"
+    body.style.backgroundColor = "#fff"
 }
